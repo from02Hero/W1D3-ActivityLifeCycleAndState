c4462e6f1a97c347fb1c81b5fa94b72e45361d64
srgarcia <srgarcia@enhanceit.com>
2020-04-09 22:01:13 -0400

    Added basic interaction

--- app/src/main/java/com/example/w1d3_activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/w1d3_activitylifecycle/MainActivity.kt
@@ -3,44 +3,19 @@ package com.example.w1d3_activitylifecycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    val TAG = "MainActivity"
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
+    fun count(view: View) {
+        counter++
+        counterTxt.text = counter.toString()
     }
 }
